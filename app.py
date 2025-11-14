46a06bee776fde4195b1ae6cbb34568287a67250
thewhatdohe <yourgithubemail@example.com>
2025-11-14 22:43:22 +0400

    Update project files and images

--- app.py
+++ app.py
@@ -6,32 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
 @app.route("/usa")
-def USA():
+def usa():
     return render_template("uae-us/us.html")
 
-
 @app.route("/uae")
-def UAE():
+def uae():
     return render_template("uae-us/uae.html")
 
-
 @app.route("/comparison")
-def Comparison():
+def comparison():
     return render_template("extra/comparison.html")
 
-
 @app.route("/conclusion")
-def Conclusion():
+def conclusion():
     return render_template("extra/conclusion.html")
 
-
 @app.route("/citations")
-def Citations():
+def citations():
     return render_template("extra/citations.html")
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
